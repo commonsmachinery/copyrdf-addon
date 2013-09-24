75f7b9e3d4e87dc0664e7677003ff4b71f116cbd
Peter Liljenberg <peter.liljenberg@gmail.com>
2013-09-24 13:25:20 +0200

    Example: paste image with attribution in text and RDFa

---
+++ example/paste.js
@@ -0,0 +1,144 @@
+// Example of pasting an image into a web page.
+
+// This code is released as public domain by Commons Machinery
+// http://commonsmachinery.se.
+
+// Author: Peter Liljenberg, peter@commonsmachinery.se
+
+DC = $rdf.Namespace('http://purl.org/dc/elements/1.1/');
+DCTERMS = $rdf.Namespace('http://purl.org/dc/terms/');
+CC = $rdf.Namespace('http://creativecommons.org/ns#');
+XHTML = $rdf.Namespace('http://www.w3.org/1999/xhtml/vocab#');
+
+$(document).ready(function() {
+   $("div").on("x-onpaste-image", function(event) {
+       var detail = event.originalEvent.detail;
+
+       // create <div><img><pre> tree to hold the info
+  
+       var div = document.createElement('div');
+
+       var img = document.createElement('img');
+       img.src = detail.image;
+       img.id = 'pasted' + detail.image.length;  // hackish ID...
+
+       div.appendChild(img);
+
+       if (detail.rdfxml) {
+	   var kb = parseRDFXML(detail.rdfxml);
+
+	   var attrib = createAttribution(kb, '', '#' + img.id);
+	   div.appendChild(attrib);
+       }
+
+       // Cram it into the target
+       detail.target.appendChild(div);
+
+       return false;
+   });
+});
+
+// Parse the RDF/XML using rdflib.js from Tabulator
+// http://www.w3.org/2005/ajar/tab
+function parseRDFXML(str) {
+    var dom = new DOMParser().parseFromString(str, 'application/xml');
+
+    var kb = new $rdf.IndexedFormula();
+    var parser = new $rdf.RDFParser(kb);
+
+    parser.parse(dom, '', null);
+
+    return kb;
+}
+
+// Get CC-related properties about the work
+function createAttribution(kb, srcURI, targetURI) {
+    var div = document.createElement('div');
+    div.setAttribute('about', targetURI);
+
+    // Collect the attribution properties about this work
+
+    var root = kb.sym(srcURI);
+
+    // Could go look for different titles and choose one based on language,
+    // and also look for dcterms:title.
+    var title = kb.any(root, DC('title'));
+
+    var attributionURL = kb.any(root, CC('attributionURL'));
+    if (attributionURL != null)
+	attributionURL = attributionURL.uri;
+    
+    var attributionName = kb.any(root, CC('attributionName'));
+
+    if (!attributionName)
+	attributionName = attributionURL;
+
+    // Use the first of xhtml:license, dcterms:license and cc:license
+
+    var license = kb.any(root, XHTML('license'));
+    if (license == null)
+	license = kb.any(root, DCTERMS('license'));
+    if (license == null)
+	license = kb.any(root, CC('license'));
+
+    if (license != null)
+	license = license.uri;
+
+    // TODO: get sources
+
+    // Build attribution
+
+    if (title) {
+	div.appendChild(createSpan('"'));
+	div.appendChild(createSpan(title, 'http://purl.org/dc/elements/1.1/title'));
+	div.appendChild(createSpan('" '));
+    }
+    else {
+	div.appendChild(createSpan('This image'));
+    }
+
+    if (attributionName) {
+	div.appendChild(createSpan(' by '));
+	if (attributionURL) {
+	    div.appendChild(createA(attributionURL, attributionName,
+				    'http://creativecommons.org/ns#attributionURL',
+				    'http://creativecommons.org/ns#attributionName'));
+	}
+	else {
+	    div.appendChild(createSpan(attributionName,
+				       'http://creativecommons.org/ns#attributionName'));
+	}
+    }
+
+    if (license) {
+	div.appendChild(createSpan(' is licensed with '));
+	div.appendChild(createA(license, license, 'license', null));
+    }
+
+    return div;
+}
+
+
+function createSpan(content, property) {
+    var span = document.createElement('span');
+    span.appendChild(document.createTextNode(content));
+    if (property)
+	span.setAttribute('property', property);
+
+    return span;
+}
+
+function createA(href, content, rel, property) {
+    var a = document.createElement('a');
+
+    a.href = href;
+    a.appendChild(document.createTextNode(content));
+    
+    if (rel)
+	a.setAttribute('rel', rel);
+
+    if (property)
+	a.setAttribute('property', property);
+
+    return a;
+}    
